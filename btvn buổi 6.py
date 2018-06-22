fe5eade5e352ad4ec4f49be7cdc66ecbdca69172
NAMHOANGLE05 <namhoangle01@gmail.com>
2018-06-22 16:43:47 +0700

    first commit

---
+++ btvn buổi 6.py
@@ -0,0 +1,34 @@
+# Object 1: Snake
+# Features 1:
+# x: int
+# y: int
+# list = [ 0 , 0, 0, 0, 0 ]
+# Method 1 :
+# def Init
+# Draw(): print list
+# Move(): x, y
+# Add(): add to list
+#
+# Object 2:Food
+# Features 2:
+# x: int
+# y: int
+# Method 2:
+# def Init
+# Draw: print("0")
+#
+# Object 3: Game
+# Features 3:
+# Food(pos): int
+# Snake(pygame.draw.rect(display_surf)
+# Stopbutton(): List
+# Continuebutton(): List
+# Endbutton(): List
+#
+# Method 3:
+# Draw()
+# Renewfood(): x pos, y pos
+# Random(): x pos, y pos
+# Eatwall(): Boolean
+# Eatself(): Boolean
+# Eatfood(): Boolean
